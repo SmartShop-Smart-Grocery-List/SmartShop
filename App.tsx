2169248ccff34ddc5ef86d68b1c9f3fb16d6957a
Jeffreyp1 <carrjeffrey12270@gmail.com>
2024-03-13 23:39:54 -0700

    finished log page

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from  '@react-navigation/bottom-tabs';
-import HomePage from './src/components/HomePage';
 import LogPage from './src/components/LogPage';
 import NamePage from './src/components/NamePage';
 import Dashboard from './src/components/Dashboard';
@@ -14,7 +13,6 @@ function MyTabs() {
   return (
       <Tab.Navigator>
         <Tab.Screen name = "Dashboard" component = {Dashboard}/>
-        <Tab.Screen name = "Home" component = {HomePage}/>
         <Tab.Screen name = "Log" component = {LogPage}/>
       </Tab.Navigator>
   )
